cb002bc173a2b86a8920d58f029088ebc9d4a4cb
ravonhenson <ravonhenson@gmail.com>
2023-11-27 14:55:05 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# steps:
+# record 
+# launch ball
+# measure distance
+# record data 
+# export data to excel
+# plot 
+
+#things i probably need
+# open cv 
+# numpy 
+# csv 
+# excel something something something
+
+print("Hello world")
