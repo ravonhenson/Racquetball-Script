1e29a4a037d4bda361840d0579cb8008bc4ef0ab
Ravon Henson <ravonhenson@gmail.com>
2023-11-27 15:44:21 -0500

    basic webcam capture

--- main.py
+++ main.py
@@ -12,4 +12,17 @@
 # csv 
 # excel something something something
 
-print("Hello world")
+import numpy as np 
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = camera.read()
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
